e93a50dd82c53be48a91dd7b92af12f14c77608a
rimsky-yamatov <dev.nox.lang@gmail.com>
2026-09-20 18:10:51 +0900

    update div,mod

--- lib/hyperint/arithmetic.rb
+++ lib/hyperint/arithmetic.rb
@@ -72,14 +72,27 @@ class HyperInt
     check_type(other)
     raise ZeroDivisionError, "divided by 0" if other.zero?
 
-    HyperInt.new(to_i / other.to_i)
+    quotient, = divide_magnitudes(@digits, other.digits)
+
+    if quotient == [0]
+      return HyperInt.zero
+    end
+
+    sign = @sign == other.sign ? 1 : -1
+    build_from_digits(sign, quotient)
   end
 
   def mod(other)
     check_type(other)
     raise ZeroDivisionError, "divided by 0" if other.zero?
 
-    HyperInt.new(to_i % other.to_i)
+    _, remainder = divide_magnitudes(@digits, other.digits)
+
+    if remainder == [0]
+      return HyperInt.zero
+    end
+
+    build_from_digits(@sign, remainder)
   end
 
   def pow(other)
@@ -144,6 +157,71 @@ class HyperInt
     end
 
     0
+  end
+
+  def divide_magnitudes(dividend, divisor)
+    return [[0], [0]] if compare_magnitudes(dividend, divisor) < 0
+
+    quotient = Array.new(dividend.length, 0)
+    remainder = [0]
+
+    (dividend.length - 1).downto(0) do |i|
+      remainder.unshift(dividend[i])
+      normalize_digits(remainder)
+
+      low = 0
+      high = BASE - 1
+
+      while low <= high
+        middle = (low + high) / 2
+        product = multiply_magnitude_by_small(divisor, middle)
+        comparison = compare_magnitudes(product, remainder)
+
+        if comparison <= 0
+          low = middle + 1
+        else
+          high = middle - 1
+        end
+      end
+
+      digit = high
+
+      if digit > 0
+        product = multiply_magnitude_by_small(divisor, digit)
+        remainder = subtract_magnitudes(remainder, product)
+      end
+
+      quotient[i] = digit
+    end
+
+    normalize_digits(quotient)
+    normalize_digits(remainder)
+
+    [quotient, remainder]
+  end
+
+  def multiply_magnitude_by_small(digits, multiplier)
+    return [0] if multiplier == 0
+
+    result = []
+    carry = 0
+
+    digits.each do |digit|
+      value = digit * multiplier + carry
+      result << value % BASE
+      carry = value / BASE
+    end
+
+    result << carry if carry > 0
+    result
+  end
+
+  def normalize_digits(digits)
+    digits.pop while digits.length > 1 && digits[-1] == 0
+
+    digits << 0 if digits.empty?
+
+    digits
   end
 
   def build_from_digits(sign, digits)
